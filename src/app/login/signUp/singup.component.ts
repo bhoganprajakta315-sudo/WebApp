722a724da8e6dfe786d1d4dda8c11fbc996ed230
bhoganprajakta315-sudo <bhoganprajakta315@gmail.com>
2025-08-15 19:42:55 +0530

    Initial commit

---
+++ src/app/login/signUp/singup.component.ts
@@ -0,0 +1,10 @@
+import { Component, } from '@angular/core';
+@Component({
+  templateUrl: './singup.component.html',
+   styleUrls:['./singup.component.css']
+})
+export class SingUpComponent {
+  constructor() {
+  }
+
+}
